a452be89537569e9b4f3254e18fec7070ee2176c
T Lankreijer <tjalling.lankreijer@gmail.com>
2021-03-10 21:20:23 +0100

    Add project files.

---
+++ Controllers/XLSXImporterController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExcelImportText.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class XLSXImporterController : ControllerBase
+    {
+        private readonly ILogger<XLSXImporterController> logger;
+
+        public XLSXImporterController(ILogger<XLSXImporterController> logger)
+        {
+            this.logger = logger;
+        }
+
+    }
+}
